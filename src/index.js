6a509fc0f8fceb151183aa97b12d1b9b71cafd1e
MarznaHuzei <marinka777xx@gmail.com>
2023-11-22 11:36:49 +0100

    fetch

--- src/index.js
+++ src/index.js
@@ -17,31 +17,40 @@ breedSelect.classList.add('is-hidden');
 loader.classList.add('is-hidden');
 error.classList.add('is-hidden');
 catInfo.classList.add('is-hidden');
+
 let arrBreedsId = [];
+
+loader.classList.replace('is-hidden', 'loader');
 
 fetchBreeds()
   .then(data => {
     data.forEach(element => {
       arrBreedsId.push({ text: element.name, value: element.id });
     });
+
     new SlimSelect({
       select: breedSelect,
       data: arrBreedsId,
     });
+
+    loader.classList.replace('loader', 'is-hidden');
+    breedSelect.addEventListener('change', onSelectBreed);
+    breedSelect.classList.replace('is-hidden', 'breed-select');
   })
   .catch(onFetchError);
-
-breedSelect.addEventListener('change', onSelectBreed);
 
 function onSelectBreed(event) {
   loader.classList.replace('is-hidden', 'loader');
   breedSelect.classList.replace('is-hidden', 'breed-select');
   catInfo.classList.add('is-hidden');
+
   const breedId = event.currentTarget.value;
+
   fetchCatByBreed(breedId)
     .then(data => {
       loader.classList.replace('loader', 'is-hidden');
       breedSelect.classList.remove('is-hidden');
+
       const { url, breeds } = data[0];
 
       catInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
@@ -53,6 +62,7 @@ function onSelectBreed(event) {
 function onFetchError() {
   breedSelect.classList.remove('is-hidden');
   loader.classList.replace('loader', 'is-hidden');
+
   iziToast.error({
     message:
       'Oops! Something went wrong! Try reloading the page or select another cat breed!',
